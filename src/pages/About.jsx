bdfa0f58c35e892de5cce655314f6325d4981ec4
Deepthi Maria Chalackal <dmc.141592@gmail.com>
2026-06-20 21:26:09 +0200

    changed pictures

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,2 +1,8 @@
 import AboutSection from '../components/AboutSection/AboutSection'
 export default function About() { return <AboutSection /> }
+
+<img
+  src="/images/about.jpg"
+  alt="About Us"
+  className="about-banner"
+/>
