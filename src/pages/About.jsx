212f21f0fe87a0850e2f2ddd19ed1ed9cd7597e4
Deepthi Maria Chalackal <dmc.141592@gmail.com>
2026-06-20 22:07:33 +0200

    about us angepasst- Grid

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,16 @@
 import AboutSection from '../components/AboutSection/AboutSection'
 export default function About() { return <AboutSection /> }
 
-<img
-  src="/images/about.jpg"
-  alt="About Us"
-  className="about-banner"
-/>
+<div className="split-grid">
+  <div>
+    ...
+  </div>
+
+  <div className="feature-image-wrapper">
+     <img
+    src="/images/about.jpg"
+    className="feature-image"
+    alt="About"
+  />
+  </div>
+</div>
